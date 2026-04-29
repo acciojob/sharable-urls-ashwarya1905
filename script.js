8e7dea4d5af83e4afc94305ce03c557b138c244d
ashwarya dhote <ashwaryadhote6@gmail.com>
2026-04-29 13:34:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// your code here
+
+    button.addEventListener("click", function () {
+      let name = document.getElementById("name").value;
+      let year = document.getElementById("year").value;
+
+      let baseURL = "https://localhost:8080/";
+      let query = [];
+
+      if (name) query.push("name=" + encodeURIComponent(name));
+      if (year) query.push("year=" + encodeURIComponent(year));
+
+      let finalURL = baseURL;
+
+      if (query.length > 0) {
+        finalURL += "?" + query.join("&");
+      }
+
+      document.getElementById("url").innerText = finalURL;
+    });
